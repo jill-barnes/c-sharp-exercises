42c416438f98245e8efaa82a0f9a863628ef6e59
Jill Graves <jill.karen.graves@gmail.com>
2020-06-13 20:27:21 -0500

    Queue peek and enqueue

---
+++ Queuing/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Queuing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue MyQueue = new Queue();
+
+            MyQueue.PrintTheQueue();
+
+            MyQueue.Enqueue(":)");
+
+            MyQueue.PrintTheQueue();
+
+            MyQueue.Enqueue(":(");
+
+            MyQueue.PrintTheQueue();
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Queuing/Queue.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Queuing
+{
+	public class Queue
+	{
+		private string[] QueueArray;
+
+		public string Peek()
+		{
+			return QueueArray[0];
+		}
+
+		public void Enqueue(string NewItem)
+        {
+			string[] EmbiggenedQueue = new string[QueueArray.Length + 1];
+			EmbiggenedQueue[EmbiggenedQueue.Length - 1] = NewItem;
+			for(int i = 0; i < QueueArray.Length; i++)
+            {
+				EmbiggenedQueue[i] = QueueArray[i];
+            }
+			QueueArray = EmbiggenedQueue;
+        }
+
+		public void Dequeue(string DeletedItem)
+        {
+			//:)
+        }
+
+		public Queue()
+		{
+			QueueArray = new string[0];
+		}
+
+		public void PrintTheQueue()
+        {
+			Console.WriteLine("I am printing");
+			for(int i = 0; i < QueueArray.Length; i++)
+            {
+                Console.WriteLine(QueueArray[i]);
+            }
+        }
+	}
+
+}
