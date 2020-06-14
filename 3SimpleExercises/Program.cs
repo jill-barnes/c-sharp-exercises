648824659566e021b929d5083a18d77fc4e946fd
Jill Graves <jill.karen.graves@gmail.com>
2020-06-14 12:46:52 -0500

    Temp Converter

---
+++ 3SimpleExercises/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _3SimpleExercises
+{
+    class Program
+
+    {
+        public static void Main(string[] args)
+        {
+            double newFahrenheit = TempConverter.CtoF(30);
+            double roundedFahrenheit = Math.Round(newFahrenheit, 2);
+            Console.WriteLine(roundedFahrenheit);
+
+            double newCelsius = TempConverter.FtoC(75);
+            double roundedCelsius = Math.Round(newCelsius, 2);
+            Console.WriteLine(roundedCelsius);
+
+            newCelsius = TempConverter.FtoC(105);
+            roundedCelsius = Math.Round(newCelsius, 2);
+            Console.WriteLine(roundedCelsius);
+
+            newFahrenheit = TempConverter.CtoF(12);
+            roundedFahrenheit = Math.Round(newFahrenheit, 2);
+            Console.WriteLine(roundedFahrenheit);
+        }
+    }
+}
---
+++ 3SimpleExercises/TempConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3SimpleExercises
+{
+    class TempConverter
+    {
+        public static double FtoC(int F)
+        {
+            double Fahrenheit = (F - 32) * (0.55556);
+            return Fahrenheit;
+        }
+
+        public static double CtoF(int C)
+        {
+            double Celsius = (C * 1.8) + 32;
+            return Celsius;
+        }
+
+    }
+}
+
